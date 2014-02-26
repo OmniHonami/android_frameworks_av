fe784cb604326feccd81ada6b707af95603499b7
ApurupaPattapu <apurupa@codeaurora.org>
2014-02-26 14:14:36 -0800

    libstagefright: Set is_streaming flag for all widevine content
    - For widevine clips played from sdcard get the file type
      from extractor's meta data.
    - By setting is_streaming flag 2k buffer size will be used
      for audio offload.
    
    CRs-Fixed: 615323
    
    Change-Id: Ic54a642aa70c3355583807eb4ca2034e4207dfd4

--- media/libstagefright/include/AwesomePlayer.h
+++ media/libstagefright/include/AwesomePlayer.h
@@ -397,6 +397,7 @@ private:
     status_t selectTrack(size_t trackIndex, bool select);
 
     size_t countTracks() const;
+    bool isWidevineContent() const;
 
 #ifdef QCOM_HARDWARE
     bool inSupportedTunnelFormats(const char * mime);
